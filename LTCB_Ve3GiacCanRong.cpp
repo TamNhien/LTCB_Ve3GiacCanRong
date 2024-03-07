3a363a1479f1e98968ed44c8d704fd2ce0ecfcde
TamNhien <canhthien91@gmail.com>
2024-03-07 19:27:06 +0700

    Add project files.

---
+++ LTCB_Ve3GiacCanRong.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Nhap chieu cao tam giac: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        cout << string(n - i, ' ') << (i == 1 || i == n ? string(2 * i - 1, '*') : '*' + string(2 * i - 3, ' ') + '*') << endl;
+    }
+
+    return 0;
+}
